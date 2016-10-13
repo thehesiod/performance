58f64ea1f3f3089c4bd3c050e257da247795fa21
Alexander Mohr <amohr@farmersbusinessnetwork.com>
2016-10-12 19:36:20 -0700

    queue speed test enhancements
    make testing two-way and simplify code

--- queue_test.py
+++ queue_test.py
@@ -7,6 +7,20 @@ import pickle
 import zmq
 import traceback
 import posix_ipc
+import sysv_ipc
+import threading
+
+from multiprocessing.heap import BufferWrapper
+
+# Monkeypatch for: https://bugs.python.org/issue28053
+from multiprocessing.connection import _ConnectionBase
+from multiprocessing.reduction import ForkingPickler
+def _conn_base_send(self, obj):
+    self._check_closed()
+    self._check_writable()
+    self._send_bytes(ForkingPickler.dumps(obj, pickle.HIGHEST_PROTOCOL))
+
+_ConnectionBase.send = _conn_base_send
 
 try:
     import nanomsg
@@ -15,226 +29,357 @@ except:
     ENABLE_NANO = False
 
 doc = {
-    'something': 'More',
+    'something': 'More' * 50,
     'another': 'thing',
     'what?': range(200),
     'ok': ['asdf', 'asdf', 'asdf']
 }
 
-num_messages = 500000
-zmq_socket = "ipc:///tmp/zmq_test"
-nano_socket = "ipc:///tmp/nano_test"
-ipc_name = 'ipc_test'
+num_messages = 250000
+zmq_socket_in = "ipc:///tmp/zmq_test_in"
+zmq_socket_out = "ipc:///tmp/zmq_test_out"
+nano_socket_out = "ipc:///tmp/nano_test_out"
+nano_socket_in = "ipc:///tmp/nano_test_in"
+ipc_name_out = 'ipc_test_out'
+ipc_name_in = 'ipc_test_in'
 
+sysv_ipc_key_out = 1
+sysv_ipc_key_in = 1
 
-def ipc_worker():
-    mq = posix_ipc.MessageQueue(ipc_name)
+def print_exc(func):
+    def func_wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except:
+            traceback.print_exc()
+            raise
+
+    return func_wrapper
+
+@print_exc
+def posix_ipc_worker(rdy_evt: multiprocessing.Event):
+    mq_in = posix_ipc.MessageQueue(ipc_name_out)
+    mq_out = posix_ipc.MessageQueue(ipc_name_in)
+
+    rdy_evt.set()
+
     try:
         size_messages = 0
         for task_nbr in range(num_messages):
-            message, priority = mq.receive()
+            message, _ = mq_in.receive()
             message = pickle.loads(message)
+
+            # pickle back to send pyobj
+            message = pickle.dumps(message, pickle.HIGHEST_PROTOCOL)
+            mq_out.send(message)
             size_messages += len(message)
     finally:
-        mq.close()
+        mq_in.close()
 
     print("Total size:", size_messages)
-    sys.exit(1)
 
 
-def queue_worker(q):
+@print_exc
+def sysv_ipc_worker(rdy_evt: multiprocessing.Event):
+    mq_in = sysv_ipc.MessageQueue(sysv_ipc_key_out)
+    mq_out = sysv_ipc.MessageQueue(sysv_ipc_key_in)
+
+    rdy_evt.set()
+
+    try:
+        size_messages = 0
+        for task_nbr in range(num_messages):
+            message, _ = mq_in.receive()
+            message = pickle.loads(message)
+
+            # pickle back to send pyobj
+            message = pickle.dumps(message, pickle.HIGHEST_PROTOCOL)
+            mq_out.send(message)
+            size_messages += len(message)
+    finally:
+        mq_in.remove()
+        mq_out.remove()
+        pass
+
+    print("Total size:", size_messages)
+
+@print_exc
+def queue_worker(q_in, q_out, rdy_evt: multiprocessing.Event):
     size_messages = 0
-    is_joinable = q.__class__ == multiprocessing.JoinableQueue().__class__
+    is_joinable = q_in.__class__ == multiprocessing.JoinableQueue().__class__
+
+    rdy_evt.set()
 
     for task_nbr in range(num_messages):
-        message = q.get()
+        message = q_in.get()
         if is_joinable:
-            q.task_done()
+            q_in.task_done()
 
+        q_out.put(message)
         size_messages += len(message)
 
     print("Total size:", size_messages)
-    sys.exit(1)
 
 
-def pipe_worker(q):
+@print_exc
+def pipe_worker(q_in, q_out, rdy_evt: multiprocessing.Event):
     size_messages = 0
+    rdy_evt.set()
+
     for task_nbr in range(num_messages):
-        message = q.recv()
+        message = q_in.recv()
+        q_out.send(message)
+
         size_messages += len(message)
 
     print("Total size:", size_messages)
-    sys.exit(1)
 
-
-def zmq_worker():
+@print_exc
+def zmq_worker(rdy_evt: multiprocessing.Event):
     context = zmq.Context()
     work_receiver = context.socket(zmq.PULL)
-    work_receiver.connect(zmq_socket)
+    work_receiver.connect(zmq_socket_out)
+
+    work_sender = context.socket(zmq.PUSH)
+    work_sender.connect(zmq_socket_in)
+
+    rdy_evt.set()
 
     size_messages = 0
     for task_nbr in range(num_messages):
-        message = work_receiver.recv()
-        message = pickle.loads(message)
+        message = work_receiver.recv_pyobj()
+        work_sender.send_pyobj(message, protocol=pickle.HIGHEST_PROTOCOL)
         size_messages += len(message)
 
     print("Total size:", size_messages)
-    sys.exit(1)
 
 
-def nano_worker():
-    try:
-        socket = nanomsg.Socket(nanomsg.PULL)
-        socket.connect(nano_socket)
+@print_exc
+def nano_worker(rdy_evt: multiprocessing.Event):
+    socket_in = nanomsg.Socket(nanomsg.PULL)
+    socket_in.connect(nano_socket_out)
 
-        size_messages = 0
-        for task_nbr in range(num_messages):
-            message = socket.recv()
-            message = pickle.loads(message)
-            size_messages += len(message)
+    socket_out = nanomsg.Socket(nanomsg.PUSH)
+    socket_out.connect(nano_socket_in)
 
-        print("Total size:", size_messages)
-    except:
-        traceback.print_exc()
+    rdy_evt.set()
 
-    sys.exit(1)
+    size_messages = 0
+    for task_nbr in range(num_messages):
+        message = socket_in.recv()
+        message = pickle.loads(message)
+
+        # we pickle back to simulate sending a python object
+        message = pickle.dumps(message, pickle.HIGHEST_PROTOCOL)
+        socket_out.send(message)
+
+        size_messages += len(message)
+
+    print("Total size:", size_messages)
 
 
-def ipc_test():
-    print("IPC")
-    mq = posix_ipc.MessageQueue(ipc_name, flags=os.O_CREAT | os.O_EXCL)
-    try:
-        proc = multiprocessing.Process(target=ipc_worker, args=())
-        proc.start()
-        time.sleep(2)
+def wait_on_sender_receiver(sender, receiver, proc_method, *proc_args):
+    rdy_evt = multiprocessing.Event()
+    proc = multiprocessing.Process(target=proc_method, args=proc_args + tuple([rdy_evt]))
+    proc.start()
+    rdy_evt.wait()
 
-        start_time = time.time()
-        for num in range(num_messages):
-            message = pickle.dumps(doc)
-            mq.send(message)
+    t1 = threading.Thread(target=sender)
+    t2 = threading.Thread(target=receiver)
 
-        proc.join()
-    finally:
-        mq.close()
-        mq.unlink()
+    start_time = time.time()
+    t1.start()
+    t2.start()
 
+    t2.join()  # wait for receiver to finish
     end_time = time.time()
+
+    t1.join()
+    proc.join()
+
     duration = end_time - start_time
     msg_per_sec = num_messages / duration
 
     print("Duration: %s" % duration)
-    print("Messages Per Second: %s" % msg_per_sec)
+    print("Messages Per Second: %s" % msg_per_sec + os.linesep)
+
+def posix_ipc_test():
+    print("Posix IPC")
+    mq_out = posix_ipc.MessageQueue(ipc_name_out, flags=os.O_CREAT | os.O_EXCL)
+    mq_in = posix_ipc.MessageQueue(ipc_name_in, flags=os.O_CREAT | os.O_EXCL)
+
+    try:
+        def sender():
+            for num in range(num_messages):
+                message = pickle.dumps(doc, pickle.HIGHEST_PROTOCOL)
+                mq_out.send(message)
+
+        def receiver():
+            for num in range(num_messages):
+                message, _ = mq_in.receive()
+                message = pickle.loads(message)
+
+        wait_on_sender_receiver(sender, receiver, posix_ipc_worker)
+    finally:
+        mq_out.close()
+        mq_in.close()
+        mq_out.unlink()
+        mq_in.unlink()
+
+
+def sysv_ipc_test():
+    print("SYSV IPC")
+
+    try:
+        mq_out = sysv_ipc.MessageQueue(sysv_ipc_key_out, flags=os.O_CREAT | os.O_EXCL)
+        mq_in = sysv_ipc.MessageQueue(sysv_ipc_key_in, flags=os.O_CREAT | os.O_EXCL)
+
+        @print_exc
+        def sender():
+            for num in range(num_messages):
+                message = pickle.dumps(doc, pickle.HIGHEST_PROTOCOL)
+                mq_out.send(message)
+
+        @print_exc
+        def receiver():
+            for num in range(num_messages):
+                message, _ = mq_in.receive()
+                message = pickle.loads(message)
+
+        wait_on_sender_receiver(sender, receiver, sysv_ipc_worker)
+    finally:
+        # mq.remove()
+        pass
+
+
+# def mp_buf_test():
+#     print("Multiprocessing Buffer")
+#     proc = multiprocessing.Process(target=sysv_ipc_worker, args=())
+#     proc.start()
+#     time.sleep(2)
+#
+#     start_time = time.time()
+#     for num in range(num_messages):
+#         message = pickle.dumps(doc)
+#         mq.send(message)
+#
+#     proc.join()
 
 
 def zmq_test():
     print("ZMQ")
+
+    files = [p.replace('ipc://', '') for p in [zmq_socket_in, zmq_socket_out]]
+    for f in files:
+        if os.path.exists(f): os.unlink(f)
+
     context = zmq.Context()
-    socket = context.socket(zmq.PUSH)
-    socket.bind(zmq_socket)
+    socket_out = context.socket(zmq.PUSH)
+    socket_out.bind(zmq_socket_out)
 
-    proc = multiprocessing.Process(target=zmq_worker, args=())
-    proc.start()
-    time.sleep(2)
+    socket_in = context.socket(zmq.PULL)
+    socket_in.bind(zmq_socket_in)
 
-    start_time = time.time()
-    for num in range(num_messages):
-        message = pickle.dumps(doc)
-        socket.send(message)
+    @print_exc
+    def sender():
+        for num in range(num_messages):
+            socket_out.send_pyobj(doc, protocol=pickle.HIGHEST_PROTOCOL)
 
-    proc.join()
+    @print_exc
+    def receiver():
+        for num in range(num_messages):
+            socket_in.recv_pyobj()
 
-    end_time = time.time()
-    duration = end_time - start_time
-    msg_per_sec = num_messages / duration
-
-    print("Duration: %s" % duration)
-    print("Messages Per Second: %s" % msg_per_sec)
+    wait_on_sender_receiver(sender, receiver, zmq_worker)
 
 
 def nano_test():
     print("Nano")
-    proc = multiprocessing.Process(target=nano_worker, args=())
-    proc.start()
 
-    # strangely the PULL has to connect before the PUSH
-    socket = nanomsg.Socket(nanomsg.PUSH)
-    socket.bind(nano_socket)
+    files = [p.replace('ipc://', '') for p in [nano_socket_in, nano_socket_out]]
+    for f in files:
+        if os.path.exists(f): os.unlink(f)
 
-    start_time = time.time()
-    for num in range(num_messages):
-        message = pickle.dumps(doc)
-        socket.send(message)
-    proc.join()
+    # strangely with nano the connect needs to happen before the bind
+    @print_exc
+    def sender():
+        socket_out = nanomsg.Socket(nanomsg.PUSH)
+        socket_out.bind(nano_socket_out)
 
-    end_time = time.time()
-    duration = end_time - start_time
-    msg_per_sec = num_messages / duration
+        for num in range(num_messages):
+            message = pickle.dumps(doc, pickle.HIGHEST_PROTOCOL)
+            socket_out.send(message)
 
-    print("Duration: %s" % duration)
-    print("Messages Per Second: %s" % msg_per_sec)
+    @print_exc
+    def receiver():
+        socket_in = nanomsg.Socket(nanomsg.PULL)
+        socket_in.bind(nano_socket_in)
+
+        for num in range(num_messages):
+            message = socket_in.recv()
+            message = pickle.loads(message)
+
+    wait_on_sender_receiver(sender, receiver, nano_worker)
 
 
 def queue_test():
     print("MP Queue")
     send_q = multiprocessing.Queue()
-    proc = multiprocessing.Process(target=queue_worker, args=(send_q,))
-    proc.start()
+    recv_q = multiprocessing.Queue()
 
-    start_time = time.time()
-    for num in range(num_messages):
-        send_q.put(doc)
+    @print_exc
+    def sender():
+        for num in range(num_messages):
+            send_q.put(doc)
 
-    proc.join()
-    end_time = time.time()
-    duration = end_time - start_time
-    msg_per_sec = num_messages / duration
+    @print_exc
+    def receiver():
+        for num in range(num_messages):
+            recv_q.get()
 
-    print("Duration: %s" % duration)
-    print("Messages Per Second: %s" % msg_per_sec)
+    wait_on_sender_receiver(sender, receiver, queue_worker, send_q, recv_q)
 
 
 def joinable_queue_test():
     print("Joinable Queue")
     send_q = multiprocessing.JoinableQueue()
-    proc = multiprocessing.Process(target=queue_worker, args=(send_q,))
-    proc.start()
+    recv_q = multiprocessing.JoinableQueue()
 
-    start_time = time.time()
-    for num in range(num_messages):
-        send_q.put(doc)
+    @print_exc
+    def sender():
+        for num in range(num_messages):
+            send_q.put(doc)
 
-    proc.join()
+    @print_exc
+    def receiver():
+        for num in range(num_messages):
+            recv_q.get()
+
+    wait_on_sender_receiver(sender, receiver, queue_worker, send_q, recv_q)
     send_q.join()
-
-    end_time = time.time()
-    duration = end_time - start_time
-    msg_per_sec = num_messages / duration
-
-    print("Duration: %s" % duration)
-    print("Messages Per Second: %s" % msg_per_sec)
 
 
 def pipe_test():
     print("MP Pipe")
     receive_conn, send_conn = multiprocessing.Pipe(False)
-    proc = multiprocessing.Process(target=pipe_worker, args=(receive_conn,))
-    proc.start()
+    receive_conn2, send_conn2 = multiprocessing.Pipe(False)
 
-    start_time = time.time()
-    for num in range(num_messages):
-        send_conn.send(doc)
+    @print_exc
+    def sender():
+        for num in range(num_messages):
+            send_conn.send(doc)
 
-    proc.join()
-    end_time = time.time()
-    duration = end_time - start_time
-    msg_per_sec = num_messages / duration
+    @print_exc
+    def receiver():
+        for num in range(num_messages):
+            receive_conn2.recv()
 
-    print("Duration: %s" % duration)
-    print("Messages Per Second: %s" % msg_per_sec)
+    wait_on_sender_receiver(sender, receiver, pipe_worker, receive_conn, send_conn2)
 
 
 def main():
-    ipc_test()
+    # sysv_ipc_test()
+    posix_ipc_test()
     queue_test()
     joinable_queue_test()
     pipe_test()
